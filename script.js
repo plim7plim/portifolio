db1aad821e60afda228e7d4dea5f49d80e7588c9
Plínio Peixoto <pliniobr33@gmail.com>
2026-07-30 13:59:15 -0300

    projeto

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ button.addEventListener("click", () => {
     }
 
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+document.querySelectorAll("#menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        menu.classList.remove("active");
+
+    });
+
+});
