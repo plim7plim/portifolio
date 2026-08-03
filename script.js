948e7c10119e3e4061aa6eb60412ab07fe1c458d
Plínio Peixoto <pliniobr33@gmail.com>
2026-08-03 10:08:55 -0300

    db

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    orderBy,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSfhfv6ZKtkDTJi-bIaFInPQD4tojvKl0",
+    authDomain: "portifolioplinio.firebaseapp.com",
+    projectId: "portifolioplinio",
+    storageBucket: "portifolioplinio.firebasestorage.app",
+    messagingSenderId: "971642836528",
+    appId: "1:971642836528:web:c4c1f6b2dafb2573ddfff5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
 const button = document.getElementById("themeButton");
 
 button.addEventListener("click", () => {
@@ -58,3 +83,72 @@ filters.forEach(filter => {
     });
 
 });
+
+const btn = document.getElementById("enviar");
+
+btn.addEventListener("click", async () => {
+
+    const nome = document.getElementById("nome").value.trim();
+
+    const mensagem = document.getElementById("mensagem").value.trim();
+
+    if(nome === "" || mensagem === ""){
+
+        alert("Preencha todos os campos.");
+
+        return;
+
+    }
+
+    await addDoc(collection(db,"comentarios"),{
+
+        nome,
+
+        mensagem,
+
+        data: serverTimestamp()
+
+    });
+
+    document.getElementById("nome").value="";
+
+    document.getElementById("mensagem").value="";
+
+    carregarComentarios();
+
+});
+
+async function carregarComentarios(){
+
+    const container = document.getElementById("comentarios");
+
+    container.innerHTML="";
+
+    const q = query(
+        collection(db,"comentarios"),
+        orderBy("data","desc")
+    );
+
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach(doc=>{
+
+        const comentario = doc.data();
+
+        container.innerHTML += `
+
+            <div class="comentario">
+
+                <h3>${comentario.nome}</h3>
+
+                <p>${comentario.mensagem}</p>
+
+            </div>
+
+        `;
+
+    });
+
+}
+
+carregarComentarios();
